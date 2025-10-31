ec0011973388ac10e8e79621d6cb664611651e67
Rohan S <113523636+rohhxn@users.noreply.github.com>
2025-10-31 08:47:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,22 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-// We will pass these from the Jenkinsfile!
-const version = process.env.APP_VERSION || '1.0 (Blue)';
-const backgroundColor = process.env.BACKGROUND_COLOR || 'lightblue';
+// This will be set by our Jenkinsfile
+const version = process.env.APP_VERSION || 'local dev'; 
+const backgroundColor = process.env.BACKGROUND_COLOR || 'grey';
 
 app.get('/', (req, res) => {
-  res.send(`
-    <body style="background-color: ${backgroundColor}; color: black; text-align: center; font-family: sans-serif;">
-      <h1>Hello from the ${version} deployment!</h1>
-    </body>
-  `);
+  res.send(`
+    <body style="background-color: ${backgroundColor}; color: white; text-align: center; font-family: sans-serif; padding-top: 50px;">
+      <h1>Hello from the ${version} deployment!</h1>
+    </body>
+  `);
 });
 
 app.get('/health', (req, res) => {
-  res.status(200).send('OK');
+  res.status(200).send('OK');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
